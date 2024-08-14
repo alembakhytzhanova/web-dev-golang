392c597fcd3443644805bedbbbdfd8650b6be373
mirvision <florangorr@gmail.com>
2024-08-14 22:37:25 +0500

    parsefile

---
+++ 03_christmas/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("letter.phpasp")
+	if err != nil {
+		fmt.Println("There was an error parsing file", err)
+	}
+	friends := []string{"Alex", "Conor", "Ken", "Ronnie", "Patick", "Nina", "Jeremy", "Gentry", "Christian"}
+
+	err = tpl.Execute(os.Stdout, friends)
+	if err != nil {
+		fmt.Println("There was an error executing template", err)
+	}
+}
